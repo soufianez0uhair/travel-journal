61b53c6a7af03e73b2688f6bd9a4fba1cb3cd095
Soufiane Zouhair <soufianezouhaironline@gmail.com>
2022-03-21 12:40:44 +0100

    Add header component and style it

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import {GoGlobe} from 'react-icons/go'
+export default function Header() {
+    return (
+        <header className="header">
+            <GoGlobe className="icon--globe"/> my travel journal
+        </header>
+    )
+}
